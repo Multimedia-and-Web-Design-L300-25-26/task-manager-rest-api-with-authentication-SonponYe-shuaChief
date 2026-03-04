2059c1a74f0d3c62b479c11c988b3c030ebb1649
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-03-04 15:36:40 +0000

    authcontoller done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,52 +1,44 @@
 import User from '../models/User.js';
 
+
 export const register = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-
     const userExists = await User.findOne({ email });
     if (userExists) {
-      return res.status(409).json({ 
-        message: 'chale the user laready exists'
-     });
+      return res.status(409).json({ message: 'User already exists' });
     }
 
-   // this is to create a new user in the database if user does not exist 
-    const user = await User.create({
-      email,
-      password,
+    const user = await User.create({ email, password });
+
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
     });
-
- 
-    if (user) {
-      res.status(201).json({
-        _id: user._id,
-        email: user.email,
-      });
-    }
   } catch (error) {
-    res.status(500).json({
-         message: 'Server error during registration'
-        
-        });
+    res.status(500).json({ message: 'Server error during registration' });
   }
 };
 
-
 export const login = async (req, res) => {
-    try {
-      const{email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-      const user = await User.findOne({ email });
+ 
+    const user = await User.findOne({ email });
 
-      if (!user) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-
-            
-        }
-        catch (error) {
-            res.status(500).json({ message: 'Server error during login' });
-        }
-    };
+   
+    if (user && (await user.matchPassword(password))) {
+      res.status(200).json({
+        _id: user._id,
+        email: user.email,
+    
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error during login' });
+  }
+};
